64b3159616e22e15dcb4ec53d759abeb06a6f1e8
IronManYG <hussain.yg2010@gmail.com>
2020-06-11 11:29:50 +0300

    Add variables that will be used in methods

--- app/src/main/java/com/example/popularmovies/NetworkUtils.java
+++ app/src/main/java/com/example/popularmovies/NetworkUtils.java
@@ -1,4 +1,23 @@
 package com.example.popularmovies;
 
-public class NetworkUtils {
+/**
+ * These utilities will be used to communicate with the themoviedb servers.
+ */
+public final class NetworkUtils {
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String MOVIES_BASE_URL = "http://api.themoviedb.org/3/movie";
+
+    private static final String apiKey = "";
+
+    private static final String TOP_RATED_MOVIES = "top_rated";
+
+    private static final String POPULAR_MOVIES = "popular";
+
+    private static final String MOVIE_POSTER_BASE_URL = "http://image.tmdb.org/t/p";
+
+    private static  String MOVIE_POSTER_SIZE = "w185";
+
+    private static  String MOVIE_POSTER_PATH_FROM_QUERY = "";
+    
 }
