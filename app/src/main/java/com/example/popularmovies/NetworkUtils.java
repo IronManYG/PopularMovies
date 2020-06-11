78682a2e80e1ce39b0ad3c4120b51f8e4d4444e7
IronManYG <hussain.yg2010@gmail.com>
2020-06-11 11:53:20 +0300

    Add getResponseFromHttpUrl

--- app/src/main/java/com/example/popularmovies/NetworkUtils.java
+++ app/src/main/java/com/example/popularmovies/NetworkUtils.java
@@ -3,8 +3,12 @@ package com.example.popularmovies;
 import android.net.Uri;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * These utilities will be used to communicate with the themoviedb servers.
@@ -50,5 +54,31 @@ public final class NetworkUtils {
         Log.v(TAG, "Built URI " + url);
 
         return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
