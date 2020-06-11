9c990a09e23fedbe2d8abadb39c94d2e3be411d5
IronManYG <hussain.yg2010@gmail.com>
2020-06-11 11:49:57 +0300

    Add buildUrl method

--- app/src/main/java/com/example/popularmovies/NetworkUtils.java
+++ app/src/main/java/com/example/popularmovies/NetworkUtils.java
@@ -1,5 +1,11 @@
 package com.example.popularmovies;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * These utilities will be used to communicate with the themoviedb servers.
  */
@@ -8,7 +14,9 @@ public final class NetworkUtils {
 
     private static final String MOVIES_BASE_URL = "http://api.themoviedb.org/3/movie";
 
-    private static final String apiKey = "";
+    private static final String KEY_PARAM = "api_key";
+
+    private static final String API_KEY = "";
 
     private static final String TOP_RATED_MOVIES = "top_rated";
 
@@ -19,5 +27,28 @@ public final class NetworkUtils {
     private static  String MOVIE_POSTER_SIZE = "w185";
 
     private static  String MOVIE_POSTER_PATH_FROM_QUERY = "";
-    
+
+    /**
+     * Builds the URL used to talk to the themoviedb servers.
+     *
+     * @return The URL to use to query the themoviedb servers.
+     */
+
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
+                .appendPath(TOP_RATED_MOVIES)
+                .appendQueryParameter(KEY_PARAM, API_KEY)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+    }
 }
