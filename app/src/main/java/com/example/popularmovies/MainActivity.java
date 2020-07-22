0b44ca058f0d2c1b902ea5a03342521935b3b15c
IronManYG <hussain.yg2010@gmail.com>
2020-07-22 21:05:16 +0300

    Set up adapter & error message & progressbar

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+
+    private MovieAdapter mMovieAdapter;
+
+    private TextView mErrorMessageDisplay;
+
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
          * do things like set the adapter of the RecyclerView and toggle the visibility.
          */
         mRecyclerView = findViewById(R.id.recyclerview_movies);
+
+        /* This TextView is used to display errors and will be hidden if there are no errors */
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
 
         /*
          * GridLayoutManager can support HORIZONTAL or VERTICAL orientations. The reverse layout
@@ -36,5 +47,23 @@ public class MainActivity extends AppCompatActivity {
          * change the child layout size in the RecyclerView
          */
         mRecyclerView.setHasFixedSize(true);
+
+        /*
+         * The MovieAdapter is responsible for linking our Movie data with the Views that
+         * will end up displaying our Movie data.
+         */
+        mMovieAdapter = new MovieAdapter (this);
+
+        /* Setting the adapter attaches it to the RecyclerView in our layout. */
+        mRecyclerView.setAdapter(mMovieAdapter);
+
+        /*
+         * The ProgressBar that will indicate to the user that we are loading data. It will be
+         * hidden when no data is loading.
+         *
+         * Please note: This so called "ProgressBar" isn't a bar by default. It is more of a
+         * circle. We didn't make the rules (or the names of Views), we just follow them.
+         */
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
     }
 }
