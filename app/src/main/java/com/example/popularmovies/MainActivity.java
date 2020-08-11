aa3677213d2740c7abe745176e6aca48810ee7ef
IronManYG <hussain.yg2010@gmail.com>
2020-08-11 18:02:42 +0300

    Add methods & make it work adapter

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.popularmovies.MovieAdapter.MovieAdapterOnClickHandler;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements MovieAdapterOnClickHandler {
 
     private RecyclerView mRecyclerView;
 
@@ -17,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mErrorMessageDisplay;
 
     private ProgressBar mLoadingIndicator;
+
+    private String mSortBy = "top_rated";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.recyclerview_movies);
 
         /* This TextView is used to display errors and will be hidden if there are no errors */
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
 
         /*
          * GridLayoutManager can support HORIZONTAL or VERTICAL orientations. The reverse layout
@@ -65,5 +79,137 @@ public class MainActivity extends AppCompatActivity {
          * circle. We didn't make the rules (or the names of Views), we just follow them.
          */
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        /* Once all of our views are setup, we can load the weather data. */
+        loadMovieData();
+    }
+
+    /**
+     * This method will get the user's preferred location for weather, and then tell some
+     * background method to get the weather data in the background.
+     */
+    private void loadMovieData() {
+        showMovieDataView();
+
+        new FetchWeatherTask().execute();
+    }
+
+    /**
+     * This method is overridden by our MainActivity class in order to handle RecyclerView item
+     * clicks.
+     *
+     * @param weatherForDay The weather for the day that was clicked
+     */
+    @Override
+    public void onClick(String weatherForDay) {
+        Context context = this;
+        Class destinationClass = DetailActivity.class;
+        Intent intentToStartDetailActivity = new Intent(context, destinationClass);
+        intentToStartDetailActivity.putExtra(DetailActivity.EXTRA_STRING,weatherForDay);
+        startActivity(intentToStartDetailActivity);
+    }
+
+    /**
+     * This method will make the View for the weather data visible and
+     * hide the error message.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showMovieDataView() {
+        /* First, make sure the error is invisible */
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        /* Then, make sure the weather data is visible */
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the error message visible and hide the weather
+     * View.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showErrorMessage() {
+        /* First, hide the currently visible data */
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        /* Then, show the error */
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+
+
+    public class FetchWeatherTask extends AsyncTask<Void , Void, String[]> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String[] doInBackground(Void... voids) {
+
+            URL movieRequestUrl = NetworkUtils.buildUrl(mSortBy);
+            Log.i("MainActivity", "Move Path :" + movieRequestUrl);
+
+            try {
+                String jsonMovieResponse = NetworkUtils.getResponseFromHttpUrl(movieRequestUrl);
+
+                String[] simpleJsonMovieData = TheMovieDbJsonUtils.parseMoviesInfoStringsFromJson(MainActivity.this ,jsonMovieResponse);
+
+                //URL moviePosterPath = NetworkUtils.buildMoviePosterUrl(simpleJsonMovieData.getPosterPath());
+
+                //Log.i("Main Activity", "The Path : " + moviePosterPath);
+
+                return simpleJsonMovieData;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+       @Override
+        protected void onPostExecute(String[] MovieData) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (MovieData != null) {
+                showMovieDataView();
+                mMovieAdapter.setMovieData(MovieData);
+            } else {
+                showErrorMessage();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.movie_sort, menu);
+        /* Return true so that the menu is displayed in the Toolbar */
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.most_popular) {
+            mSortBy = "popular";
+            mMovieAdapter.setMovieData(null);
+            loadMovieData();
+            return true;
+        }
+
+        if (id == R.id.top_rated) {
+            mSortBy = "top_rated";
+            mMovieAdapter.setMovieData(null);
+            loadMovieData();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
