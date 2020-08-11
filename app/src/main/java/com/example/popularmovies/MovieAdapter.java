b425a631f50fe3903ab7f6850395b15b22ae6872
IronManYG <hussain.yg2010@gmail.com>
2020-08-11 18:05:12 +0300

    Add methods to it

--- app/src/main/java/com/example/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/popularmovies/MovieAdapter.java
@@ -1,4 +1,135 @@
 package com.example.popularmovies;
 
-public class MovieAdapter {
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * {@link MovieAdapter} exposes a list of Movie details to a
+ * {@link androidx.recyclerview.widget.RecyclerView}
+ */
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
+
+    private String[] mMovieData;
+
+    private Movie mMovie;
+
+    /*
+     * An on-click handler that we've defined to make it easy for an Activity to interface with
+     * our RecyclerView
+     */
+    private final MovieAdapterOnClickHandler mClickHandler;
+
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface MovieAdapterOnClickHandler {
+        void onClick(String weatherForDay);
+    }
+
+    /**
+     * Creates a MovieAdapter.
+     *
+     * @param clickHandler The on-click handler for this adapter. This single handler is called
+     *                     when an item is clicked.
+     */
+    public MovieAdapter(MovieAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
+
+    /**
+     * Cache of the children views for a movie_sort list item.
+     */
+    public class MovieAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final ImageView mMovieImageView;
+
+        public MovieAdapterViewHolder(View view) {
+            super(view);
+            mMovieImageView = (ImageView) view.findViewById(R.id.movie_pic);
+            view.setOnClickListener(this);
+        }
+
+        /**
+         * This gets called by the child views during a click.
+         *
+         * @param v The View that was clicked
+         */
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            String MovieByOrder = mMovieData[adapterPosition];
+            mClickHandler.onClick(MovieByOrder);
+        }
+    }
+    /**
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param viewGroup The ViewGroup that these ViewHolders are contained within.
+     * @param viewType  If your RecyclerView has more than one type of item (which ours doesn't) you
+     *                  can use this viewType integer to provide a different layout. See
+     *                  {@link androidx.recyclerview.widget.RecyclerView.Adapter#getItemViewType(int)}
+     *                  for more details.
+     * @return A new ForecastAdapterViewHolder that holds the View for each list item
+     */
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.movie_list_itme;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new MovieAdapterViewHolder(view);
+    }
+
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the weather
+     * details for this particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param movieAdapterViewHolder The ViewHolder which should be updated to represent the
+     *                                  contents of the item at the given position in the data set.
+     * @param position                  The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(MovieAdapterViewHolder movieAdapterViewHolder, int position) {
+        String posterPath = mMovieData[position];
+        mMovie = new Movie(posterPath);
+        String UriPosterPath = String.valueOf(NetworkUtils.buildMoviePosterUrl(mMovie.getPosterPath()));
+        Log.i("Movie Adapter", "The Path : " + UriPosterPath);
+        Picasso.get().load(UriPosterPath).into(movieAdapterViewHolder.mMovieImageView);
+    }
+
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our Movies
+     */
+    @Override
+    public int getItemCount() {
+        if (null == mMovieData) return 0;
+        return mMovieData.length;
+    }
+
+    /**
+     * This method is used to set the weather forecast on a ForecastAdapter if we've already
+     * created one. This is handy when we get new data from the web but don't want to create a
+     * new ForecastAdapter to display it.
+     *
+     * @param MovieData The new weather data to be displayed.
+     */
+    public void setMovieData(String[] MovieData) {
+        mMovieData = MovieData;
+        notifyDataSetChanged();
+    }
 }
